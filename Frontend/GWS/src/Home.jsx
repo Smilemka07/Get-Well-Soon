a56332d77a35b659082c4c1df8a09185f7d1b641
Muskan Aggarwal <smileworld782@gmail.com>
2026-03-27 12:21:04 +0530

    feat: update Home component structure and styling

--- Frontend/GWS/src/Home.jsx
+++ Frontend/GWS/src/Home.jsx
@@ -1,5 +1,37 @@
-function Home(){
-    return (<h1> Hey...</h1>);
+import "./Home.css";
+import { images } from "./assets/images";
+function Home() {
+  return (
+    <div class="parent">
+      <div id="nav">
+        <h1>GSW.</h1>
+        <a href="./calendar.html">
+          <img src={images.calendar} alt="Calendar" class="calendar" />
+        </a>
+      </div>
+      <div id="box">
+        <div id="listForm">
+          <div class="formBanner">
+            <h2>Get well soon!</h2>
+            <img
+              src={images.Capsules}
+              alt="Capsules"
+              class="capsules imageHalf"
+            />
+            <img
+              src={images.capsulesfull}
+              alt="Capsules"
+              class="capsules imageFull"
+            />
+          </div>
+          <ul id="container" class="invisible-scrollbar"></ul>
+          <button id="add">
+            <i class="fas fa-plus"></i> New Med
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
---
+++ Frontend/GWS/src/assets/images.js
@@ -0,0 +1,7 @@
+import calendar from "./calendar.png";
+import Capsules from "./Capsules.png";
+import capsulesfull from "./capsulesfull.png";
+
+export const images = {
+ calendar, Capsules , capsulesfull
+};
