a27423d414804af55ffa7d5b7ff277aa9c1f09d5
Muskan Aggarwal <smileworld782@gmail.com>
2026-04-01 15:24:41 +0530

    feat: implement database connection and upsert

--- Backend/app.js
+++ Backend/app.js
@@ -1,14 +1,32 @@
 import express from "express";
 import cors from "cors";
+import pg from "pg";
+import dotenv from "dotenv";
 const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(cors());
+dotenv.config();
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
-app.post("/addTask", (req, res) => {
-  const { taskDescription } = req.body;
-  console.log("Received:", taskDescription);
-  res.json({ message: "Task added" });
+db.connect();
+
+app.post("/tasks", async (req, res) => {
+  const { task_description } = req.body;
+  try {
+    const query = `INSERT INTO listItems (task_description) VALUES ($1)`;
+    await db.query(query, [task_description]);
+    res.json({ message: "Task added" });
+  } catch (err) {
+    console.error("List error", err);
+    res.status(500).send("Error adding the listItem");
+  }
 });
 
 app.listen(port, () => {
--- Frontend/GWS/src/Home.jsx
+++ Frontend/GWS/src/Home.jsx
@@ -1,19 +1,26 @@
 import "./Home.css";
 import { Link } from "react-router-dom";
 import { images } from "./assets/images";
+import { useState, useEffect } from "react";
 
 function Home() {
+  //put tasks in database
+  const [task, setTask] = useState("");
   const addTask = async () => {
-    await fetch("http://localhost:3000/addTask", {
+    await fetch("http://localhost:3000/tasks", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        taskDescription: "My Task",
+        task_description: task,
       }),
     });
+    setTask("");
   };
+
+  //Get tasks to display on screen
+  const listItems = [1, 2, 3, 4, 5, 6, 7, 8, 2, 3, 4, 5, 6, 7, 8];
   return (
     // Home page structure
     <div className="parent">
@@ -41,7 +48,12 @@ function Home() {
               className="capsules imageFull"
             />
           </div>
-          <ul id="container" className="invisible-scrollbar"></ul>
+          <ul id="container" className="invisible-scrollbar">
+            {listItems.map((item) => (
+              <li>{item}</li>
+            ))}
+          </ul>
+          <input className="input" value={task} onChange={(e) => setTask(e.target.value)} placeholder="example: Paracetin-500, 1 tablet - Morning/Night" />
           <button id="add" onClick={addTask}>
             <i className="fas fa-plus"></i> New Med
           </button>
