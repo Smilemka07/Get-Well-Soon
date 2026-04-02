9f5d5cf1c7e6294f0ef394932f0d091e3824a846
Muskan Aggarwal <smileworld782@gmail.com>
2026-04-02 10:36:09 +0530

    feat: Add toggle checkpoints functionality

--- Frontend/GWS/src/Home.jsx
+++ Frontend/GWS/src/Home.jsx
@@ -33,52 +33,80 @@ function Home() {
   useEffect(() => {
     fetchTasks();
   }, []);
-
+  //function to check mark and cut the task as done
+  const [completed, setCompleted] = useState([]);
+  function checkMark(id) {
+    setCompleted((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  }
   return (
     // Home page structure
     <div className="parent">
       {/* nav */}
-      
+
       <div id="nav">
         <h1 className="inner">GSW.</h1>
         <Link to="/Calendar">
           <img src={images.calendar1} alt="Calendar" className="calendar" />
         </Link>
-      
       </div>
 
       {/* box in the centre */}
       <div className="inner">
-      <div id="box">
-        <div id="listForm">
-          <div className="formBanner">
-            <h2>Get well soon!</h2>
-            <img
-              src={images.Capsules}
-              alt="Capsules"
-              className="capsules imageHalf"
+        <div id="box">
+          <div id="listForm">
+            <div className="formBanner">
+              <h2>Get well soon!</h2>
+              <img
+                src={images.Capsules}
+                alt="Capsules"
+                className="capsules imageHalf"
+              />
+              <img
+                src={images.capsulesfull}
+                alt="Capsules"
+                className="capsules imageFull"
+              />
+            </div>
+            <ul id="container" className="invisible-scrollbar">
+              {listItems.map((item) => (
+                <li
+                  key={item.serial_number}
+                  style={{
+                    textDecoration: completed[item.serial_number]
+                      ? "line-through"
+                      : "none",
+                  }}
+                >
+                  <button
+                    onClick={() => checkMark(item.serial_number)}
+                    className="checkbutton"
+                  >
+                    <img
+                      src={
+                        completed[item.serial_number]
+                          ? images.FinishtaskIcon
+                          : images.ActivetaskIcon
+                      }
+                      alt="Checkbutton"
+                    />
+                  </button>
+                  {item.task_description}
+                </li>
+              ))}
+            </ul>
+            <input
+              className="input"
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
+              placeholder="example: Paracetin-500, 1 tablet - Morning/Night"
             />
-            <img
-              src={images.capsulesfull}
-              alt="Capsules"
-              className="capsules imageFull"
-            />
+            <button disabled={!task.trim()} id="add" onClick={addTask}>
+              <i className="fas fa-plus"></i> New Med
+            </button>
           </div>
-          <ul id="container" className="invisible-scrollbar">
-            {listItems.map((item) => (
-              <li key={item.serial_number}>{item.task_description}</li>
-            ))}
-          </ul>
-          <input
-            className="input"
-            value={task}
-            onChange={(e) => setTask(e.target.value)}
-            placeholder="example: Paracetin-500, 1 tablet - Morning/Night"
-          />
-          <button disabled={!task.trim()} id="add" onClick={addTask}>
-            <i className="fas fa-plus"></i> New Med
-          </button>
-        </div>
         </div>
       </div>
     </div>
--- Frontend/GWS/src/assets/images.js
+++ Frontend/GWS/src/assets/images.js
@@ -1,7 +1,9 @@
 import calendar1 from "./calendar1.png";
 import Capsules from "./Capsules.png";
 import capsulesfull from "./capsulesfull.png";
+import ActivetaskIcon from "./ActivetaskIcon.png";
+import FinishtaskIcon from "./FinishtaskIcon.png";
 
 export const images = {
- calendar1, Capsules , capsulesfull
+ calendar1, Capsules , capsulesfull, ActivetaskIcon, FinishtaskIcon
 };
